6559d7cdbeff3117450c75be91e143516ff26163
Gowri-Krishnamoorthi <Gowrikrishnamoorthi14@gmail.com>
2025-03-31 19:31:29 +0530

    31-03-2025

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'socialproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Ensure this includes your template directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect  # Import redirect
 from .views import user_login, user_logout, home
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('', lambda request: redirect('login')),  # Redirect /users/ to /users/login/
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('home/', home, name='home'),
+    path('password_change/',auth_view.PasswordChangeView.as_view(template_name ='users/password_change.html'), name='password_change'),
 ]
